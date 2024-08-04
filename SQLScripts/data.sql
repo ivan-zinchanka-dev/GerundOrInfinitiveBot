c365b1fbb8d2f832f1f32e97d08ec9746850bca8
Ivan Zinchanka <75093640+ivan-zinchanka-dev@users.noreply.github.com>
2024-08-04 20:45:41 +0300

    more data

--- SQLScripts/data.sql
+++ SQLScripts/data.sql
@@ -17,13 +17,37 @@ INSERT INTO [Examples] ([SourceSentence], [UsedWord], [CorrectAnswer])
 VALUES ('We can''t help ... when my dad tries to speak French. His accent is awful!', 'laugh', 'laughing');
 
 INSERT INTO [Examples] ([SourceSentence], [UsedWord], [CorrectAnswer])
+VALUES ('We carried on ... until about 2.00 in the morning', 'chat', 'chatting');
+
+INSERT INTO [Examples] ([SourceSentence], [UsedWord], [CorrectAnswer])
+VALUES ('Miriam denied ... her husband, but the jury didn''t believe her', 'kill', 'killing');
+
+INSERT INTO [Examples] ([SourceSentence], [UsedWord], [CorrectAnswer])
+VALUES ('I used to enjoy ..., but now I don''t', 'fly', 'flying');
+
+INSERT INTO [Examples] ([SourceSentence], [UsedWord], [CorrectAnswer])
+VALUES ('Do you fancy ... a film this evening?', 'see', 'seeing');
+
+INSERT INTO [Examples] ([SourceSentence], [UsedWord], [CorrectAnswer])
+VALUES ('I don''t feel like ... out tonight', 'go', 'going');
+
+INSERT INTO [Examples] ([SourceSentence], [UsedWord], [CorrectAnswer])
+VALUES ('Have you finished ... the report yet?', 'write', 'writing');
+
+INSERT INTO [Examples] ([SourceSentence], [UsedWord], [CorrectAnswer])
+VALUES ('Karen has given up ... meat, but she still eats fish', 'eat', 'eating');
+
+INSERT INTO [Examples] ([SourceSentence], [UsedWord], [CorrectAnswer])
 VALUES ('I can''t afford ... on holiday this summer.', 'go', 'to go');
 
 INSERT INTO [Examples] ([SourceSentence], [UsedWord], [CorrectAnswer])
-VALUES ('They have decided ... off the wedding.', 'call', 'to call');
+VALUES ('I have agreed ... David back the money he lent me next week.', 'pay', 'to pay');
 
 INSERT INTO [Examples] ([SourceSentence], [UsedWord], [CorrectAnswer])
-VALUES ('I have agreed ... David back the money he lent me next week.', 'pay', 'to pay');
+VALUES ('The results appear ... the scientist''s theory', 'support', 'to support');
+
+INSERT INTO [Examples] ([SourceSentence], [UsedWord], [CorrectAnswer])
+VALUES ('They have decided ... off the wedding.', 'call', 'to call');
 
 
 GO
