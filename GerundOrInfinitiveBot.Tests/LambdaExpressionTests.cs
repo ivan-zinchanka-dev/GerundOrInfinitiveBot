09f7257261e9c4976acaf60bba7cb8c29bb9f4e2
Ivan Zinchanka <75093640+ivan-zinchanka-dev@users.noreply.github.com>
2025-05-03 00:00:48 +0300

    foo

---
+++ GerundOrInfinitiveBot.Tests/LambdaExpressionTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+namespace GerundOrInfinitiveBot.Tests;
+
+[TestFixture]
+public class LambdaExpressionTests
+{
+    [Test]
+    public void TestLambdaExpression()
+    {
+        Expression<Func<int, int>> expression = x => x * 5;
+        Assert.That(expression.Body.ToString(), Is.EqualTo("(x * 5)"));
+        Assert.That(expression.Body.NodeType, Is.EqualTo(ExpressionType.Multiply));
+        
+        var binaryExp = (BinaryExpression)expression.Body;
+        Assert.That(binaryExp.Left.ToString(), Is.EqualTo("x"));
+        Assert.That(binaryExp.Right.ToString(), Is.EqualTo("5"));
+    }
+}
