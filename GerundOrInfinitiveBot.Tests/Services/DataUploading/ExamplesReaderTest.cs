a8f328492e0390da13ed97da903f0742a951607c
Ivan Zinchanka <75093640+ivan-zinchanka-dev@users.noreply.github.com>
2025-07-16 22:26:28 +0300

    feat: examples reader, docker

---
+++ GerundOrInfinitiveBot.Tests/Services/DataUploading/ExamplesReaderTest.cs
@@ -0,0 +1,19 @@
+﻿using GerundOrInfinitiveBot.Models.DataBaseObjects;
+using GerundOrInfinitiveBot.Services.DataLoading;
+
+namespace GerundOrInfinitiveBot.Tests.Services.DataUploading;
+
+[TestFixture]
+public class ExamplesReaderTest
+{
+    [Test]
+    public async Task PrintExamples()
+    {
+        IEnumerable<Example> examples = await new ExamplesReader().UploadExamplesAsync();
+
+        foreach (Example example in examples)
+        {
+            Console.WriteLine($"'{example.SourceSentence}', '{example.UsedWord}', '{example.CorrectAnswer}', '{example.AlternativeCorrectAnswer}'");
+        }
+    }
+}
---
+++ GerundOrInfinitiveBot/Services/DataLoading/ExamplesReader.cs
@@ -0,0 +1,76 @@
+﻿using System.Text.RegularExpressions;
+using GerundOrInfinitiveBot.Models.DataBaseObjects;
+
+namespace GerundOrInfinitiveBot.Services.DataLoading;
+
+internal class ExamplesReader
+{
+    private const string SourceDataFileName = "source.edm";
+    
+    public async Task<IEnumerable<Example>> UploadExamplesAsync()
+    {
+        var examples = new List<Example>();
+        string sourceDataFilePath = Path.Combine(Directory.GetCurrentDirectory(), SourceDataFileName);
+        
+        using (var reader = new StreamReader(sourceDataFilePath))
+        {
+            string currentLine;
+            while ((currentLine = await reader.ReadLineAsync()) != null)
+            {
+                if (TryReadExample(currentLine, out Example example))
+                {
+                    examples.Add(example);
+                }
+            }
+        }
+
+        return examples;
+    }
+    
+    private bool TryReadExample(string line, out Example example)
+    {
+        if (string.IsNullOrEmpty(line) || 
+            string.IsNullOrWhiteSpace(line) || 
+            line.StartsWith("/*") ||
+            line.EndsWith("*/"))
+        {
+            example = null;
+            return false;
+        }
+
+        MatchCollection matches = Regex.Matches(line, "'((?:[^']|'')*)'");
+        var propertyValues = new List<string>();
+        
+        foreach (Match match in matches)
+        {
+            string propertyValue = match.Groups[1].Value;
+            propertyValue = propertyValue.Replace("''", "'");
+
+            propertyValues.Add(propertyValue);
+        }
+
+        if (propertyValues.Any())
+        {
+            example = new Example()
+            {
+                SourceSentence = GetValueByIndex(propertyValues, 0),
+                UsedWord = GetValueByIndex(propertyValues, 1),
+                CorrectAnswer = GetValueByIndex(propertyValues, 2),
+                AlternativeCorrectAnswer = GetValueByIndex(propertyValues, 3),
+            };
+
+            return true;
+        }
+        else {
+            
+            example = null;
+            return false;
+        }
+    }
+
+    private string GetValueByIndex(List<string> propertyValues, int index)
+    {
+        return propertyValues.Count > index ? propertyValues[index] : null;
+    }
+
+}
